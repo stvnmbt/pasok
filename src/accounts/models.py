74dbf45ef3da10724829142b99952075afea1e99
Steven Ambat <ambat.steven@gmail.com>
2023-10-15 14:51:00 +0800

    updated models

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -1,9 +1,8 @@
 from datetime import datetime
 from flask_login import UserMixin
 from src import bcrypt, db
-from sqlalchemy import func, Enum, Column, Integer, String, LargeBinary
+from sqlalchemy import func, Enum
 import enum
-from qrcode import QRCode
 
 class Status(enum.Enum):
     PRESENT = 'PRESENT'
@@ -19,7 +18,7 @@ class Attendance(db.Model):
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     #subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
     #section_id = db.Column(db.Integer, db.ForeignKey('section.id'), nullable=False)
-    user = db.relationship('User', back_populates='attendance')
+    user = db.relationship('User', backref='attendance')
 
 class User(UserMixin, db.Model):
     __tablename__ = "user"
@@ -30,17 +29,14 @@ class User(UserMixin, db.Model):
     first_name = db.Column(db.String(150), nullable=False)
     middle_name = db.Column(db.String(150), nullable=True)
     last_name = db.Column(db.String(150), nullable=False)
-    
-    qr_code = db.Column(db.LargeBinary, nullable=True)
-
     is_admin = db.Column(db.Boolean, nullable=False, default=False)
     is_confirmed = db.Column(db.Boolean, nullable=False, default=False)
     created_on = db.Column(db.DateTime, nullable=False)
     confirmed_on = db.Column(db.DateTime, nullable=True)
-    attendance = db.relationship('Attendance', back_populates='user')
+    qr_code = db.Column(db.LargeBinary, nullable=True)
 
     def __init__(
-        self, email, password, first_name, middle_name, last_name, attendance, qr_code, is_admin=False, is_confirmed=False, confirmed_on=None
+        self, email, password, first_name, middle_name, last_name, qr_code, is_admin=False, is_confirmed=False, confirmed_on=None
     ):  
         self.email = email
         self.password = bcrypt.generate_password_hash(password)
@@ -52,7 +48,6 @@ class User(UserMixin, db.Model):
         self.is_admin = is_admin
         self.is_confirmed = is_confirmed
         self.confirmed_on = confirmed_on
-        self.attendance = Attendance()
 
     def __repr__(self):
         return f"<email {self.email}>"
