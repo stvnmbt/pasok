a94f12d59f935743a0300c906d54638280ea12dd
Steven <ambat.steven@gmail.com>
2023-10-17 02:35:24 +0800

    cleaning up

--- src/accounts/models.py
+++ src/accounts/models.py
@@ -14,11 +14,11 @@ class Attendance(db.Model):
 
     id = db.Column(db.Integer, primary_key=True)
     attendance_status = db.Column(Enum(Status), nullable=False)
-    created = db.Column(db.DateTime(timezone=True), default=func.now(), nullable=False)
+    created = db.Column(db.DateTime(timezone=True), server_default=func.now(), nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
     #subject_id = db.Column(db.Integer, db.ForeignKey('subject.id'), nullable=False)
     #section_id = db.Column(db.Integer, db.ForeignKey('section.id'), nullable=False)
-    user = db.relationship('User', backref='attendance')
+    user = db.relationship('User', back_populates='attendance')
 
 class User(UserMixin, db.Model):
     __tablename__ = "user"
@@ -29,23 +29,22 @@ class User(UserMixin, db.Model):
     first_name = db.Column(db.String(150), nullable=False)
     middle_name = db.Column(db.String(150), nullable=True)
     last_name = db.Column(db.String(150), nullable=False)
-    is_admin = db.Column(db.Boolean, nullable=False, default=False)
+    is_faculty = db.Column(db.Boolean, nullable=False, default=False)
     is_confirmed = db.Column(db.Boolean, nullable=False, default=False)
     created_on = db.Column(db.DateTime, nullable=False)
     confirmed_on = db.Column(db.DateTime, nullable=True)
-    qr_code = db.Column(db.LargeBinary, nullable=True)
+    attendance = db.relationship('Attendance', back_populates='user')
 
     def __init__(
-        self, email, password, first_name, middle_name, last_name, qr_code, is_admin=False, is_confirmed=False, confirmed_on=None
+        self, email, password, first_name, middle_name, last_name, is_faculty=False, is_confirmed=False, confirmed_on=None
     ):  
         self.email = email
         self.password = bcrypt.generate_password_hash(password)
         self.first_name = first_name
         self.middle_name = middle_name
         self.last_name = last_name
-        self.qr_code = qr_code
         self.created_on = datetime.now()
-        self.is_admin = is_admin
+        self.is_faculty = is_faculty
         self.is_confirmed = is_confirmed
         self.confirmed_on = confirmed_on
 
