778477beebabdbd3306c95dbee97cc579b85c386
Youwonn <cyverzola69@gmail.com>
2023-10-18 12:33:42 +0800

    scanner.py works

--- src/accounts/scanner.py
+++ src/accounts/scanner.py
@@ -1,27 +1,29 @@
 import cv2
 from pyzbar.pyzbar import decode
 from threading import Thread
-from src import User, db
+from flask import Flask, render_template, redirect
+from src import User
 from src.accounts.models import Attendance, Status
 from src.accounts import create_app
+import requests
 
 # Create an instance of the Flask app
 app = create_app()
 
 class QRCodeDetector:
     def __init__(self):
-        self.camera = cv2.VideoCapture(0)  # Use 0 for the default camera (you can change this if you have multiple cameras)
+        self.camera = cv2.VideoCapture(1)  # Use 0 for the default camera (you can change this if you have multiple cameras)
         self.stopped = False
 
+    # Rest of your QRCodeDetector class
+
     @staticmethod
-    def get_qr_code_from_database(user_id):
-        # Connect to the database and retrieve the User object
-        user = User.query.get(user_id)
+    def extract_user_info_from_qr_code(qr_code_content):
+        user_id = qr_code_content[:3]
+        first_name = qr_code_content[3:6]
+        last_name = qr_code_content[6:]
 
-        # Access the QR code attribute from the User object
-        qr_code = user.qr_code
-
-        return qr_code
+        return user_id, first_name, last_name
 
     def start(self):
         Thread(target=self.detect_qr_codes, args=(), daemon=True).start()
@@ -30,40 +32,56 @@ class QRCodeDetector:
         with app.app_context():
             while True:
                 # Read the frame from the camera
-                ret, frame = self.camera.read()
+                _, frame = self.camera.read()
 
                 # Decode QR codes from the frame
                 decoded_objects = decode(frame)
 
+                # Draw rectangles around the detected QR codes
                 for obj in decoded_objects:
+                    # Extract the QR code coordinates
+                    x, y, w, h = obj.rect
+
+                    # Draw a rectangle around the QR code
+                    cv2.rectangle(frame, (x, y), (x + w, y + h), (0, 255, 0), 2)
+
+                    # Decode the QR code data
                     data = obj.data.decode("utf-8")
 
-                    # Check if the QR code data is a valid user_id
-                    try:
-                        user_id = int(data)
-                    except ValueError:
-                        continue
+                    # Process the decoded QR code data
+                    self.process_decoded_qr_code(data)
 
-                    # Retrieve the QR code from the database
-                    qr_code = self.get_qr_code_from_database(user_id)
-
-                    # Perform actions on the decoded QR code data
-                    if qr_code is not None:
-                        with app.test_request_context():
-                            with app.test_client() as client:
-                                with client.session_transaction() as session:
-                                    session["user_id"] = user_id
-
-                                response = client.post("/add")
-                                if response.status_code == 200:
-                                    print("Attendance added successfully!")
+                # Display the frame with the detected QR codes
+                cv2.imshow("QR Code Detection", frame)
 
                 if cv2.waitKey(1) & 0xFF == ord("q"):  # Press 'q' to exit
                     break
 
         cv2.destroyAllWindows()
 
-    def stop(self):
+    def process_decoded_qr_code(self, data):
+        # Check if the QR code data is a valid user_id
+        try:
+            user_id = int(data)
+        except ValueError:
+            return
+
+        # Extract user information from the QR code content
+        qr_code_content = data
+        user_id, first_name, last_name = self.extract_user_info_from_qr_code(qr_code_content)
+
+        # Retrieve the User object
+        user = User.query.get(user_id)
+
+        # Perform actions on the decoded QR code data
+        if user is not None:
+            # Make a POST request to the /add route within the 'core' blueprint of the Flask application
+            with app.test_request_context():
+                with app.test_client() as client:
+                    response = client.post("/core/add", data={"user_id": user_id}, follow_redirects=True, method="post")
+                    if response.status_code == 200:
+                        print("Attendance added successfully!")
+
         self.stopped = True
 
 if __name__ == "__main__":
--- src/core/views.py
+++ src/core/views.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template, redirect, url_for, request
+from flask import Blueprint, render_template, redirect, url_for, request, flash, jsonify
 from flask_login import login_required, current_user
 from src import db
 from src.utils.decorators import check_is_confirmed
@@ -16,14 +16,25 @@ def home():
 @core_bp.route("/add", methods=["GET", "POST"])
 @login_required
 @check_is_confirmed
-def add(page=1):
-    per_page = 15
-
+def add():
     if request.method == "POST":
-        attendance = Attendance(attendance_status=Status.PRESENT, user_id=current_user.id)
-        db.session.add(attendance)
-        db.session.commit()
+        if request.form.get("add_attendance"):
+            # Handle the form submit (button click) to add attendance
+            attendance = Attendance(attendance_status=Status.PRESENT, user_id=current_user.id)
+            db.session.add(attendance)
+            db.session.commit()
+            flash("Attendance added successfully!")
 
-    attendance_records = Attendance.query.order_by(Attendance.created.desc())
-    
+    if request.is_json:
+        # Handle the POST request from the QR code scanner to add attendance
+        data = request.get_json()
+        user_id = data.get("user_id")
+        if user_id is not None:
+            attendance = Attendance(attendance_status=Status.PRESENT, user_id=user_id)
+            db.session.add(attendance)
+            db.session.commit()
+            return jsonify({"message": "Attendance added successfully"})
+
+    attendance_records = Attendance.query.order_by(Attendance.created.desc()).all()
     return render_template("core/index.html", attendance_records=attendance_records)
+
