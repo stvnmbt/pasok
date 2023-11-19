bf68ccec9dde678e914a857a79a06f2977a0b149
Steven <ambat.steven@gmail.com>
2023-11-20 04:31:35 +0800

    added buttons and logic for present and late

--- src/utils/scanner.py
+++ src/utils/scanner.py
@@ -2,9 +2,13 @@ from sqlalchemy import delete
 from src import db
 from src.accounts.models import Attendance, Status, User
 
-def add_attendance(s):
+def add_attendance(s, isLate):
     user = User.query.get(s)
-    attendance = Attendance(attendance_status=Status.LATE, user_id=s) # ADD: Change status with variable
+    status = Status.PRESENT
+    if isLate:
+        status = Status.LATE
+
+    attendance = Attendance(attendance_status=status, user_id=s) # ADD: Change status with variable
 
     if attendance.attendance_status==Status.PRESENT:
         user.present_count += 1
@@ -15,3 +19,6 @@ def add_attendance(s):
 
     db.session.add(attendance)
     db.session.commit()
+
+def mark_absent():
+    users = db.session.query(Attendance)
