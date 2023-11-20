d12baaa5eee78215b1fdbe62bfd6d40586da48ef
Steven <ambat.steven@gmail.com>
2023-11-20 15:36:59 +0800

    fixed init

--- src/__init__.py
+++ src/__init__.py
@@ -6,17 +6,56 @@ from flask_login import LoginManager
 from flask_mail import Mail
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
+from google.cloud.sql.connector import Connector, IPTypes
+
+# initialize Python Connector object
+connector = Connector()
+
+# Python Connector database connection function
+def getconn():
+    conn = connector.connect(
+        'pasoksystem:asia-east1:pasok', # Cloud SQL Instance Connection Name
+        "pg8000",
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASS"],
+        db=os.environ["DB_NAME"],
+        ip_type= IPTypes.PUBLIC  # IPTypes.PRIVATE for private IP
+    )
+    return conn
 
 app = Flask(__name__)
+
+app.config['CSRF_ENABLED'] = True
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['BCRYPT_LOG_ROUNDS'] = 13
+app.config['WTF_CSRF_ENABLED'] = True
+app.config['SECURITY_PASSWORD_SALT'] = os.environ.get("SECURITY_PASSWORD_SALT", "very-important")
 
 login_manager = LoginManager()
 login_manager.init_app(app)
 bcrypt = Bcrypt(app)
+
+# MAIL CONFIGS
+app.config['MAIL_DEFAULT_SENDER'] = "noreply@flask.com"
+app.config['MAIL_SERVER'] = "smtp.gmail.com"
+app.config['MAIL_PORT'] = 465
+app.config['MAIL_USE_TLS'] = False
+app.config['MAIL_USE_SSL'] = True
+app.config['MAIL_DEBUG'] = False
+app.config['MAIL_USERNAME'] = os.environ["EMAIL_USER"]
+app.config['MAIL_PASSWORD'] = os.environ["EMAIL_PASSWORD"]
 mail = Mail(app)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["SQLALCHEMY_DATABASE_URI"]
+
+#app.config['SQLALCHEMY_DATABASE_URI'] = os.environ["SQLALCHEMY_DATABASE_URI"]
 app.config['SECRET_KEY'] = os.environ["SECRET_KEY"]
-connect_unix_socket()
-db = SQLAlchemy(app)
+app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql+pg8000://"
+app.config['SQLALCHEMY_ENGINE_OPTIONS'] = {
+    "creator": getconn
+}
+
+# initialize the app with the extension
+db = SQLAlchemy()
+db.init_app(app)
 migrate = Migrate(app, db)
 
 # Registering blueprints
@@ -26,7 +65,9 @@ from src.core.views import core_bp
 app.register_blueprint(accounts_bp)
 app.register_blueprint(core_bp)
 
-from src.accounts.models import User
+from src.accounts.models import User, Attendance, ClassList, Status, Semester
+with app.app_context():
+    db.create_all()
 
 login_manager.login_view = "accounts.login"
 login_manager.login_message_category = "danger"
